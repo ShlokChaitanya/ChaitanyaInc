940d65ac4bd0149721c9d6bb9baf19a553463c63
shlokchaitanya <shlokchaitanya120@gmail.com>
2023-06-24 21:05:54 +0530

    workflows changes

--- build.js
+++ build.js
@@ -1,3 +1,16 @@
-require('dotenv').config()
-console.log(process.env.NODE_ENV)
-console.log("Building...");
+const fs = require('fs');
+const secret = require('./secret.json');
+
+const firebaseConfig = {
+    apiKey: secret.API_KEY,
+    authDomain: secret.AUTH_DOMAIN,
+    projectId: secret.PROJECT_ID,
+    storageBucket: secret.STORAGE_BUCKET,
+    messagingSenderId: secret.MESSAGING_SENDER_ID,
+    appId: secret.APP_ID,
+    measurementId: secret.MEASUREMENT_ID
+};
+
+const firebaseConfigString = JSON.stringify(firebaseConfig, null, 2);
+
+fs.writeFileSync('firebaseConfig.js', `const firebaseConfig = ${firebaseConfigString};\n\nexport default firebaseConfig;\n`);
--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-
-const firebaseConfig = {
-    apiKey:  process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKE,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
-};
-
+import firebaseConfig from './firebaseConfig.js';
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const analytics = firebase.analytics();
