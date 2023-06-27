c17d8f08c135acc3174d69a33ea272edce7c877c
shlokchaitanya <shlokchaitanya120@gmail.com>
2023-06-27 21:19:09 +0530

    Inital commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const functions = require("firebase-functions");
 const express = require("express");
 const cors = require('cors');
 const app = express();
@@ -6,8 +7,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json({ extended: false }));
 app.use("/payment", require("./routes/payment"));
-app.use(cors({
-    origin: 'http://localhost:3000',
-}));
+app.get('/api', (req, res) => res.send("API Running"));
+app.get('/', (req, res) => res.send("API Running"));
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+// }));
+
 
 app.listen(port, () => console.log(`server started on port ${port}`));
+exports.app = functions.https.onRequest(app);
